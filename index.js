8e9fef18b1d69b6c45655bda193b1de549aeb8c0
Luigitime! <antoniors2003@gmail.com>
2022-10-02 12:26:10 +0200

    Add basic math functions

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return Math.floor(num1 / num2);
+}
+
+
+console.log(add(2, 1));
+console.log(subtract(5, 1));
+console.log(multiply(2, 2));
+console.log(divide(10, 3));
