f0dcef7434ff5e620754892895932c3fdf925380
Luigitime! <antoniors2003@gmail.com>
2022-10-02 12:32:43 +0200

    Add operate function

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  return Math.floor(num1 / num2);
+  return num1 / num2;
 }
 
-
-console.log(add(2, 1));
-console.log(subtract(5, 1));
-console.log(multiply(2, 2));
-console.log(divide(10, 3));
+function operate(operator, num1, num2) {
+  if (operator === '+') return add(num1, num2);
+  if (operator === '-') return subtract(num1, num2);
+  if (operator === '*') return multiply(num1, num2);
+  if (operator === '/') return divide(num1, num2);
+  return 0;
+}
