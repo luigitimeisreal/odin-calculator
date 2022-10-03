e4440fa7f2219bdad37fb9e597429ac296e3b76d
Luigitime! <antoniors2003@gmail.com>
2022-10-03 14:11:05 +0200

    Add listeners for buttons

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ function operate(operator, num1, num2) {
   if (operator === '/') return divide(num1, num2);
   return 0;
 }
+
+function evaluate(expression) {
+  let isProductOrDivision = false;
+  let isSumOrSubtract = false;
+  for (let i = 1; i < expression.length - 1; i++) {
+    if (expression[i] === '*' || expression[i] === '*') {
+      let firstIndex = i;
+      let lastIndex = i;
+      while (expression[firstIndex] !== '*' && expression[firstIndex] !== '/' && expression[firstIndex] !== '+' && expression[firstIndex] !== '-' ) {
+        firstIndex--;
+      }
+      while (expression[lastIndex] !== '*' && expression[lastIndex] !== '/' && expression[lastIndex] !== '+' && expression[lastIndex] !== '-' ) {
+        lastIndex++;
+      }
+      console.log(expression[i])
+    }
+  }
+}
+
+let expression = "";
+
+const btns = document.querySelector('.buttons');
+const display = document.querySelector('.display');
+
+for (let btnChild of btns.children) {
+  btnChild.addEventListener('click', () => {
+    if (btnChild.id === 'del') {
+      expression = expression.slice(0, -1);
+    } else if (btnChild.id === 'calc'){
+      console.log(evaluate(expression));
+    } else {
+      expression += btnChild.id;
+    }
+    display.textContent = expression;
+  })
+}
+
